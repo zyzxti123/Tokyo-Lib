bbc8efe25e53df6c5808e6e1c6bfcd10a5d16580
zyzxti123 <145914233+zyzxti123@users.noreply.github.com>
2024-03-10 21:38:10 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -27,104 +27,6 @@ local executor = (
     'unknown'
 )
 
-local Signal = {
-    new = function()
-        local self = setmetatable({}, Signal)
-    
-        self._bindableEvent = Instance.new("BindableEvent")
-        self._argMap = {}
-        self._source = ENABLE_TRACEBACK and debug.traceback() or ""
-    
-        -- Events in Roblox execute in reverse order as they are stored in a linked list and
-        -- new connections are added at the head. This event will be at the tail of the list to
-        -- clean up memory.
-        self._bindableEvent.Event:Connect(function(key)
-            self._argMap[key] = nil
-    
-            -- We've been destroyed here and there's nothing left in flight.
-            -- Let's remove the argmap too.
-            -- This code may be slower than leaving this table allocated.
-            if (not self._bindableEvent) and (not next(self._argMap)) then
-                self._argMap = nil
-            end
-        end)
-    
-        return self
-    end,
-    Fire = function(...)
-        if not self._bindableEvent then
-            warn(("Signal is already destroyed. %s"):format(self._source))
-            return
-        end
-    
-        local args = table.pack(...)
-    
-        -- TODO: Replace with a less memory/computationally expensive key generation scheme
-        local key = HttpService:GenerateGUID(false)
-        self._argMap[key] = args
-    
-        -- Queues each handler onto the queue.
-        self._bindableEvent:Fire(key)
-    end,
-    Connect = function(handler)
-        if not (type(handler) == "function") then
-            error(("connect(%s)"):format(typeof(handler)), 2)
-        end
-    
-        return self._bindableEvent.Event:Connect(function(key)
-            -- note we could queue multiple events here, but we'll do this just as Roblox events expect
-            -- to behave.
-    
-            local args = self._argMap[key]
-            if args then
-                handler(table.unpack(args, 1, args.n))
-            else
-                error("Missing arg data, probably due to reentrance.")
-            end
-        end)
-    end,
-    Once = function(handler)
-        if not (type(handler) == "function") then
-            error(("once(%s)"):format(typeof(handler)), 2)
-        end
-    
-        return self._bindableEvent.Event:Once(function(key)
-            local args = self._argMap[key]
-            if args then
-                handler(table.unpack(args, 1, args.n))
-            else
-                error("Missing arg data, probably due to reentrance.")
-            end
-        end)
-    end,
-    Wait = function()
-        local key = self._bindableEvent.Event:Wait()
-        local args = self._argMap[key]
-        if args then
-            return table.unpack(args, 1, args.n)
-        else
-            error("Missing arg data, probably due to reentrance.")
-            return nil
-        end
-    end,
-    Destroy = function()
-        if self._bindableEvent then
-            -- This should disconnect all events, but in-flight events should still be
-            -- executed.
-    
-            self._bindableEvent:Destroy()
-            self._bindableEvent = nil
-        end
-    
-        -- Do not remove the argmap. It will be cleaned up by the cleanup connection.
-    
-        setmetatable(self, nil)
-    end
-}
-local Signal = {}
-Signal.__index = Signal
-Signal.ClassName = "Signal"
-
 local library = {
     windows = {};
     indicators = {};
@@ -153,11 +55,11 @@ local library = {
     images = {
         ['gradientp90'] = 'https://raw.githubusercontent.com/portallol/luna/main/modules/gradient90.png';
         ['gradientp45'] = 'https://raw.githubusercontent.com/portallol/luna/main/modules/gradient45.png';
-        ['colorhue'] = 'https://raw.githubusercontent.com/portallol/luna/main/modules/lgbtqshit.png';
+      ['colorhue'] = 'https://raw.githubusercontent.com/portallol/luna/main/modules/lgbtqshit.png';
         ['colortrans'] = 'https://raw.githubusercontent.com/portallol/luna/main/modules/trans.png';
     };
     numberStrings = {['Zero'] = 0, ['One'] = 1, ['Two'] = 2, ['Three'] = 3, ['Four'] = 4, ['Five'] = 5, ['Six'] = 6, ['Seven'] = 7, ['Eight'] = 8, ['Nine'] = 9};
-    signal = Signal;
+    signal = loadstring(game:HttpGet("https://raw.githubusercontent.com/zyzxti123/Tokyo-Lib/main/Signal.lua"))();
     open = false;
     opening = false;
     hasInit = false;
